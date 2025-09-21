511a78b49dfad68aafb2251c9099445d1e27e361
tstyer <travisstyer.ts@gmail.com>
2025-09-21 22:31:28 +0100

    Amend current app function

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let selectedIndex = 0;
+  useState;
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found.</p>}
       <ul ClassName="list-group">
-        {items.map((item) => (
-          <li>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              selectedIndex = index;
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
